f55671ab72c7a27504bfc21cc5a5a182fa5b0210
Shvec Sergei <fejanin@mail.ru>
2024-04-06 14:00:39 +0300

    The first html template and view function have been created.

--- order_acceptance_service/settings.py
+++ order_acceptance_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'order_acceptance_service_abi.apps.OrderAcceptanceServiceAbiConfig',
 ]
 
 MIDDLEWARE = [
--- order_acceptance_service_abi/views.py
+++ order_acceptance_service_abi/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'order_acceptance_service_abi/index.html')
