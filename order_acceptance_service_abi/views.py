296cb63c1a52d7800f2b1237599d68a9370df588
Shvec Sergei <fejanin@mail.ru>
2024-04-14 11:13:13 +0300

    Creating a template filter and processing Product model data for further use in the template.

--- order_acceptance_service_abi/views.py
+++ order_acceptance_service_abi/views.py
@@ -1,15 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from order_acceptance_service_abi.models import Product
+from .utils import products_by_cat_dict, products_table_lst
 
 
 @login_required
 def start_page(request):
-    data = {'title': 'Стартовая страница'}
+    data = {
+        'title': 'Стартовая страница'}
     return render(request, 'order_acceptance_service_abi/start_page.html', data)
 
 
 @login_required
 def order(request):
-    data = {'title': 'Заказ продукции'}
+    products = Product.objects.filter(is_active=True)
+    products_by_cat = products_by_cat_dict(products)
+    result = products_table_lst(products_by_cat)
+    data = {
+        'title': 'Заказ продукции',
+        'products_by_cat': result,
+    }
     return render(request, 'order_acceptance_service_abi/order.html', data)
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
